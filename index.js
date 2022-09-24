cee373051fe7f200e1c8096c2bdcfd1dd18c793c
Alexey Shirokov <shirokovam@lad24.ru>
2022-09-24 12:51:17 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 
-const ourPlanetName = "Земля";
+// const ourPlanetName = "Земля";
 
-console.log(ourPlanetName);
+// console.log(ourPlanetName);
 
-const name = "alex";
+// const name = "Ivan";
+
+// const surName = "petrov";
+
+// const age = 46;
+
+// const userData = `Имя пользователя: ${name}, возраст ${age}`;
+
+// const userDataNew = "Имя пользователя: ${name}, возраст ${age}";
+
+// console.log(userData);
+
+// //const isUser = false;
+
+// /*const isVisible = true;
+// const isVisible2 = true; */
+
+// let townName;
+
+// console.log(townName);
+
+// townName = "Moscow";
+
+// console.log(townName);
+
+const userData = {
+  name: "Ivan",
+  age: 34,
+};
+
+console.log(userData);
+
+const num = 12345678901234567890n;
+
+console.log(num);
