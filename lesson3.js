8d4f7a934e0c7d7bcb38d0c56bf8bb012993ef69
Alexey Shirokov <shirokovam@lad24.ru>
2022-10-08 10:44:01 +0300

    условные опаераторы

---
+++ lesson3.js
@@ -0,0 +1,68 @@
+// if (true) {
+//   console.log("true");
+// }
+// if (false) {
+//   console.log("false");
+// }
+
+// const userName = "Alex3";
+
+// if (userName === "Ivan") {
+//   console.log(`Привет ${userName}`);
+// } else if (userName === "Alex") {
+//   console.log(`Привет друг ${userName}`);
+// } else if (userName === "Petr") {
+//   console.log(`Hi ${userName}`);
+// } else {
+//   console.log("Привет Незнакомец");
+// }
+
+// if ("true") {
+//   if (1) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// } else {
+//   console.log("false");
+// }
+
+// const userName = "Alex45";
+
+// const result = userName === "Alex" ? "Hello Alex" : "Hello User";
+
+// console.log(result);
+
+// const a = 100;
+// const b = 20;
+// const c = 3;
+
+// if (a < b) {
+//   if (b < c) {
+//     console.log("C самый большой");
+//   } else {
+//     console.log("B самый большой");
+//   }
+// } else {
+//   if (a < c) {
+//     console.log("C самый большой");
+//   } else {
+//     console.log("A самый большой");
+//   }
+// }
+
+const userRole = "manager";
+
+// switch (userRole) {
+//   case "admin":
+//     console.log("Это админ, и он любит чай");
+//     break;
+
+//   case "user":
+//     console.log("Это юзер, и он любит колу");
+//     break;
+
+//   case "manager":
+//     console.log("Это manager, и он любит кофе");
+//     break;
+// }
