2dd66c623eb72d30ac84af1ef5fa7627d4923e2e
Alexey Shirokov <shirokovam@lad24.ru>
2022-10-15 10:15:44 +0300

    lesson 4

---
+++ lesson4.js
@@ -0,0 +1,50 @@
+// function showMessage(message) {
+//   console.log(message);
+// }
+
+// showMessage("Hi");
+
+// showMessage("Hello");
+
+// showMessage("Привет");
+
+// showMessage("Как дела?");
+
+// function sum(a = 0, b) {
+//   console.log(a + b);
+// }
+// sum(2, 3);
+// sum(2, 0);
+// sum(2);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const result = sum(3, 5);
+
+// console.log(result);
+// console.log(sum(4, 6));
+
+// function positiveSum(a, b) {
+//   if (a > 0 && b > 0) {
+//     return a + b;
+//   }
+//   return 0;
+// }
+// console.log(positiveSum(2, 3));
+// console.log(positiveSum(-2, 3));
+
+// function getName() {
+//   const userName = "Alex";
+//   return userName;
+// }
+
+// function getUserName() {
+//   console.log(userName);
+// }
+
+// console.log(getName());
+// console.log(getUserName());
+
+
