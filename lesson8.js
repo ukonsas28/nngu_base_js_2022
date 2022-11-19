85bec94877918070fb3f93b1d110a5401efd0f99
Alexey Shirokov <shirokovam@lad24.ru>
2022-11-19 10:56:34 +0300

    proto

---
+++ lesson8.js
@@ -0,0 +1,26 @@
+// const user = {
+//   name: "Alex",
+// };
+
+// console.log(user);
+
+// const arr = [1, 2];
+// console.log(arr);
+
+// console.log(new String());
+
+const obj = {
+  city: "NN",
+};
+
+const russian = {
+  country: "Russia",
+  city: "London",
+};
+
+const people = {
+  name: "Alex",
+  age: 26,
+};
+
+console.log(people.city);
