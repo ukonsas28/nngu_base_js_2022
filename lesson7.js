5624e1935bf87460680bd671e11736fcb43239ff
Alexey Shirokov <shirokovam@lad24.ru>
2022-11-19 10:12:29 +0300

    HW 7

--- lesson7.js
+++ lesson7.js
@@ -85,36 +85,126 @@
 // greetingUser22(":)");
 // greetingUser22("((");
 
-const town = {
-  name: "Moscow",
-  population: "25 mln",
-  getName: function () {
-    console.log(this.name);
-    return this.name;
-  },
-  getPopulation: function () {
-    console.log(this.population);
-    return this.population;
-  },
-  setAnyValue: function (key, value) {
-    this[key] = value;
-  },
-};
+// const town = {
+//   name: "Moscow",
+//   population: "25 mln",
+//   getName: function () {
+//     console.log(this.name);
+//     return this.name;
+//   },
+//   getPopulation: function () {
+//     console.log(this.population);
+//     return this.population;
+//   },
+//   setAnyValue: function (key, value) {
+//     this[key] = value;
+//   },
+// };
 
-const townTwo = {
-  name: "London",
-  population: "5 mln",
-};
-console.log(townTwo);
+// const townTwo = {
+//   name: "London",
+//   population: "5 mln",
+// };
+// console.log(townTwo);
 
-town.getName();
-town.getPopulation();
-town.getName.call(townTwo);
-town.getPopulation.call(townTwo);
+// town.getName();
+// town.getPopulation();
+// town.getName.call(townTwo);
+// town.getPopulation.call(townTwo);
 
-const bound = town.setAnyValue.bind(townTwo);
+// const bound = town.setAnyValue.bind(townTwo);
 
-bound("name", "Paris");
-bound("country", "France");
+// bound("name", "Paris");
+// bound("country", "France");
 
-console.log(townTwo);
+// console.log(townTwo);
+
+// const cubeValue = (a) => {
+//   return (b, c) => {
+//     return a * b * c;
+//   };
+// };
+
+// const first = cubeValue(10);
+
+// console.log(first(10, 10));
+// console.log(first(2, 5));
+// console.log(first(5, 7));
+
+// const second = cubeValue(4);
+
+// const test = (a, b, c) => a * b * c;
+
+// console.log(second(4, 10, 10));
+// console.log(second(4, 2, 5));
+// console.log(second(4, 5, 7));
+
+// const cubeValue = (a, b, c) => a * b * c;
+
+// console.log(cubeValue(1, 3, 3));
+
+// const cubeCaryValue = (a) => {
+//   return (b) => {
+//     return (c) => {
+//       return a * b * c;
+//     };
+//   };
+// };
+
+// const cubeCaryValueVar2 = (a) => () => {
+//   //some logic a
+// };
+
+// console.log(cubeCaryValue(1)(3)(3));
+
+// const twoArgs = cubeCaryValue(1)(3);
+
+// console.log(twoArgs(3));
+// console.log(twoArgs(4));
+// console.log(twoArgs(5));
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", "");
+//   if (password == "1234") ok();
+//   else fail();
+// }
+
+// let user = {
+//   name: "Вася",
+
+//   loginOk() {
+//     alert(`${this.name} logged in`);
+//   },
+
+//   loginFail() {
+//     alert(`${this.name} failed to log in`);
+//   },
+// };
+
+// askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", "");
+//   if (password == "1234") ok();
+//   else fail();
+// }
+
+// let user = {
+//   name: "John",
+
+//   login(result) {
+//     alert(this.name + (result ? " logged in" : " failed to log in"));
+//   },
+// };
+
+// askPassword(user.login.bind(user, true), user.login.bind(user, false));
+
+const elem = { value: "Пока" };
+
+function func(surname, name) {
+  alert(`${this.value}, ${surname}, ${name}`);
+}
+
+func.call(elem, "Петров", "Петр");
+
+// func.bind(elem)("Иванов", "Иван");
